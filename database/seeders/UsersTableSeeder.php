45e862dcf9c5aa03cedd4fa75f4682798a203906
rebel <rebelprajapati88@gmail.com>
2023-02-21 11:49:23 +0545

    user table seeder created

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'email_verified_at' => Carbon::now(),
+            ]
+        ];
+
+        User::insert($users);
+    }
+}
