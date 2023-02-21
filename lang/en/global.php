7d63a7cbd90e80d8bb23a3b763511186d2967991
rebel <rebelprajapati88@gmail.com>
2023-02-21 23:52:02 +0545

    adminlte dashboard implemented

---
+++ lang/en/global.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'add'                                  => 'Add',
+    'areYouSure'                           => 'Are you sure?',
+    'create'                               => 'Add',
+    'dashboard'                            => 'Dashboard',
+    'delete'                               => 'Delete',
+    'edit'                                 => 'Edit',
+    'list'                                 => 'List',
+    'logout'                               => 'Logout',
+    'save'                                 => 'Save',
+    'show'                                 => 'Show',
+    'view'                                 => 'View',
+    'submit'                               => 'Submit',
+    'change_password'                      => 'Change Password',
+    'change_password_success'              => 'Password changed successfully',
+    'back_to_list'                         => 'Back to List',
+    'datatables'                           => [
+        'copy'          => 'Copy',
+        'csv'           => 'CSV',
+        'excel'         => 'Excel',
+        'pdf'           => 'PDF',
+        'print'         => 'Print',
+        'colvis'        => 'Column visibility',
+        'delete'        => 'Delete selected',
+        'zero_selected' => 'No rows selected',
+    ],
+
+];
